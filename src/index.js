3f44441e7fd8bc01436b3b9abd061bd3e4ce4d5c
Ihor Susoi <ihor.susoi@gmail.com>
2023-01-02 23:34:57 +0200

    add: some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
+  let a, result;
+  a = number;
+  if(number==0)return 'zero';
+  if(number<20)return numberToString(a);
+  result = '';
+  if( number / 10 >= 2 && number < 100 ) {
+    result = numberToString10((a-a%10)/10,number) + numberToString(a%10);
+  }
+  if(number > 100){
+    result = numberToString100((a-a%100)/100,number) + numberToString10((a%100-a%10)/10,number) + numberToString(a%10);
+  }
   
+  return result;
 }
+
+function numberToString(a){
+    let arr = ['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    return a==0?'':arr[a-1];
+}
+
+function numberToString10(a,space){
+    let arr=['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    return space%10 != 0 ? arr[a - 2] + ' ' : arr[a - 2];
+}
+
+function numberToString100(a,space){
+    let arr = ['one','two','three','four','five','six','seven','eight','nine'];
+    return space%100 ? arr[a-1] + ' hundred ' : arr[a-1] + 'hundred';
+}
+
